9530598eaca1c1d1fb41bd236120c56f3180c15b
Nicolas VINCENT <nico.vince@gmail.com>
2022-03-11 14:21:36 +0100

    indent/syntax: rm files shipped with vim
    perl, vhdl, and tex and now properly shipped with vim distribution and
    no longer needs to be in user config files

--- macros/gless.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Shell script to start Vim with less.vim.
-# Read stdin if no arguments were given.
-
-if test $# = 0; then
-  gvim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' -
-else
-  gvim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' "$@"
-fi
